d9a21fc1e36dc159714f399f5971c1c633197a2e
RobWiederstein <khuon68@gmail.com>
2014-12-24 07:22:43 -0600

    continue to work

--- boyle start 1.R
+++ boyle start 1.R
@@ -9,9 +9,8 @@ author <- scan(file = file,
                       strip.white = T)
 
 
-
+# set up file names--Author_Westlaw Number
 justice_number_cases <- function(justice = "boyle"){
-  # set up file names using WL numbers--get to author_"Westlaw number"
   cite <- grep("^\\(Cite as: (.*)\\)$", author, value = T)
   cite <- gsub("\\(Cite as: ", "", cite)
   cite <- gsub("^[0-9L](.*)Ky\\.\\), ", "", cite)
@@ -37,6 +36,7 @@ justice_number_cases <- function(justice = "boyle"){
   cite <- paste("boyle", cite, sep = "_")
 }
 
+#split download into cases and save
 divide_into_cases  <- function(justice = "boyle"){
   begin <- grep("^\\(Cite as: (.*)\\)$", author, value = F)
   end   <- (grep("^END OF DOCUMENT$", author, value = F))
@@ -50,7 +50,7 @@ divide_into_cases  <- function(justice = "boyle"){
       write(mycase, file = file)
   }
 }
-
+#get rid of headnotes since they are added by publisher
 strip_headnotes    <- function(justice = "boyle"){
   path  <- paste(getwd(), "boyle", sep = "/")
   files_in <- list.files(path = path, pattern = "^boyle")  #all files?? what if other files in directory?
@@ -68,10 +68,9 @@ strip_headnotes    <- function(justice = "boyle"){
 
 check_dissent <- function(justice = "boyle"){
   path  <- paste(getwd(), "boyle", sep = "/")
-  files_in <- list.files(path = path, pattern = ".txt$")  #all files?? what if other files in directory?
+  files_in <- list.files(path = path, pattern = ".txt$")  #all files ending in ".txt"
   files_out <- paste (files_in, "-out1.txt", sep = "")
   omit.case <- NULL
-  keep.case <- NULL
   i     <- NULL
   for(i in seq(along = files_in)) { # start for loop with numeric or character vector;
     # numeric vector is often more flexible
@@ -86,68 +85,8 @@ check_dissent <- function(justice = "boyle"){
   }
 }
 
-
-
-
-
-rm (file, author)
-#we have a corpus!! Is it any good?  Run it with Stylo.
-library (stylo)
-a <- stylo()
-b <- classify()
-
-
-x <- scan(paste(path, files[1], sep = "/"), what = "character", sep = "\n")
-
-x
-#check for dissent or concurrence and either delete or
-y <- regexpr("Court of Appeals of Kentucky" ,x)
-court <- substring(x, y, y + attr(y, "match.length")-1)
-y <- regexpr("(OPINION(.*)BOYLE.)|(CHIEF JUSTICE BOYLE)", x)
-
-
-
-#skip to next file
-
-#begin with delivered the opinion of the court
-
-#end with before end of document
-
-files <- list.files(path = paste(getwd(), "test", sep = "/"))
-path <- paste(getwd(), "test", sep = "/")
-cite <- NULL
-court <- NULL
-judge <- NULL
-date <-  NULL
-corpus <- NULL
-plaintiff <- NULL
-defendant <- NULL
-i <- NULL
-for(i in seq(along=files)) { # start for loop with numeric or character vector;
-                             # numeric vector is often more flexible
-  x <- scan(paste(path, files[i], sep = "/"), what = "character", sep = "\n")
-  y <- regexpr("Court of Appeals of Kentucky" ,x)
-  court[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  y <- regexpr("(OPINION(.*)BOYLE.)|(CHIEF JUSTICE BOYLE)", x)
-  judge[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  y <- regexpr("([Jj]an|[Ff]eb|[Mm]ar|[Aa]pr|[Mm]ay|[Jj]un|[Jj]ul|[Aa]ug|
-[Ss]ep|[Oo]ct|[Nn]ov|[Dd]ec)..\\d?\\d,\\s18\\d\\d", x)
-  date[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  y <- regexpr("((OPINION(.*)BOYLE.)|(CHIEF JUSTICE BOYLE))(.*)END OF DOCUMENT", x)
-  corpus[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  y <- regexpr("ky\\.([^.]*)\\.", x)
-  plaintiff[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  y <- regexpr("v\\.([^,]*)\\.", x)
-  defendant[i] <- substring(x, y, y + attr(y, "match.length")-1)
-  print(files[i], quote=F)
-  
-}
-  
-  x <- data.frame(x, sum=apply(x, 1, sum), mean=apply(x, 1, mean)) # calculates sum and mean for each data frame
-  assign(files[i], x) # generates data frame object and names it after content in variable 'i'
-  print(files[i], quote=F) # prints loop iteration to screen to check its status
-  write.table(x, paste(files[i], c(".out"), sep=""), quote=FALSE, sep="\t", col.names = NA) 
-}
-
-ls()
+#Corpus built for justice boyle.  Run it with Stylo.
+#library (stylo)
+#a <- stylo()
+#b <- classify()
 
--- string tutorial.R
+++ string tutorial.R
@@ -7,6 +7,57 @@ a <- strsplit(a, split = "", fixed = T)
 a <- letters[1:3]
 a <- unlist(str_split(a, pattern = ""))
 
+data(USArrests)
+states <- rownames(USArrests)
+substr(states, start = 1, stop = 4)
+abbreviate(states)
+names(states)
+state_chars = nchar(states)
+states[which(state_chars == max(state_chars))]
+grep("^N", states, value = T)
+grep("y$", states, value = T)
+
+positions_a <- gregexpr(pattern = "a", text = states, ignore.case = TRUE)
+num_a <- sapply(positions_a, function(x) ifelse(x[1] > 0, length(x), 0))
+num_a
+sum(num_a) #61 "a"s in the names of states
+sum (str_count(states, "a"))
+sum (str_count(tolower(states), "a")) #61
+
+#vector of vowels
+vowels <- c("a", "e", "i", "o", "u")
+# vector for storing results
+num_vowels = vector(mode = "integer", length = 5)
+# calculate number of vowels in each name
+for (j in seq_along(vowels)) {
+  num_aux = str_count(tolower(states), vowels[j])
+  num_vowels[j] = sum(num_aux) }
+# add vowel names
+names(num_vowels) = vowels
+# total number of vowels
+num_vowels
+# sort them in decreasing order
+sort(num_vowels, decreasing = TRUE)
+
+#pct. of total
+char_tot <- sum(nchar(tolower(states)))
+num_vowels/char_tot
+
+cat(rep("A", 1000), fill = 50)
+
+rot <- function(ch, k = 13) {
+  p0 <- function(...) paste(c(...), collapse = "")
+  A <- c(letters, LETTERS, " '")
+  I <- seq_len(k); chartr(p0(A), p0(c(A[-I], A[I])), ch)
+}
+pw <- "my secret pass phrase"
+(crypw <- rot(pw, 13)) #-> you can send this off
+
+## now ``decrypt'' :
+rot(crypw, 54 - 13) # -> the original:
+stopifnot(identical(pw, rot(crypw, 54 - 13)))
+
+
 file <- paste (getwd(), "frost.txt",
                sep = "/"
 )
